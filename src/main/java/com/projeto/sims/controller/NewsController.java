2411a18e2a2eaa8d11d78af2e3b424b496900828
CintiaKarolliny <cintiakarolliny.9@gmail.com>
2023-08-25 23:31:49 -0300

    criação e alteração das páginas das cidades

--- src/main/java/com/projeto/sims/controller/NewsController.java
+++ src/main/java/com/projeto/sims/controller/NewsController.java
@@ -1,14 +1,53 @@
 package com.projeto.sims.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class NewsController {
-
-	@GetMapping("/teste")
-	public String teste() {
-		return "foi?";
+	
+	@GetMapping("/bertioga")
+	public String bertioga() {
+		return "bertioga";
 	}
 	
+	@GetMapping("/cubatao")
+	public String cubatao() {
+		return "cubatao";
+	}
+	
+	@GetMapping("/guaruja")
+	public String guaruja() {
+		return "guaruja";
+	}
+	
+	@GetMapping("/itanhaem")
+	public String itanhaem() {
+		return "itanhaem";
+	}
+	
+	@GetMapping("/mongagua")
+	public String mongagua() {
+		return "mongagua";
+	}
+	
+	@GetMapping("/peruibe")
+	public String peruibe() {
+		return "peruibe";
+	}
+	
+	@GetMapping("/praiagrande")
+	public String praiagrande() {
+		return "praiagrande";
+	}
+	
+	@GetMapping("/santos")
+	public String santos() {
+		return "santos";
+	}
+	
+	@GetMapping("/saovicente")
+	public String saovicente() {
+		return "saovicente";
+	}
 }
